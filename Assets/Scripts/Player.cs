c4aec6282fdd99f30c7984d021585214a6de0b30
TheScarred <thescarred@outlook.com>
2020-02-22 03:16:36 -0600

    Controller Stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public GameObject[] characters;
+    public GameObject prefab;
+    //public PlayerController control;
     public PlayerStats stats;
+    public GameObject[] characters;
 
-    // Start is called before the first frame update
     void Start()
     {
         stats = ScriptableObject.CreateInstance<PlayerStats>();
@@ -17,9 +19,33 @@ public class Player : MonoBehaviour
         stats.rng = (float)Constants.BaseStats.RNG;
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        //control = new PlayerController();
+        //control.Player.Fire.started += ctx => Tap();
+    }
+
+    //private void OnEnable() => control.Player.Enable();
+    //private void OnDisable() => control.Player.Disable();
+
     void Update()
     {
-        
+        /*if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Debug.Log("Touch! X: " + touch.position.x);
+        }*/
+
+    }
+
+    public void Tap()
+    {
+        Debug.Log("Tap!");
+        Instantiate(prefab, transform);
+    }
+
+    public void Look()
+    {
+        Debug.Log("Look!");
     }
 }
