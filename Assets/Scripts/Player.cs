20ff0c31838140e575c4628f5a5a05e97fec9bde
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-02-22 22:10:03 -0600

    EscenaIvan
    hermososlosquiero

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,15 @@ public class Player : MonoBehaviour
     public Rigidbody rigi;
     public GameObject[] characters;
 
+
+    //animator y layer para los objetivos
+    public Animator animator;
+    public LayerMask enemyLayers;
+    public float attackRange = 0.5f;
+
+    [SerializeField]
+    Transform attackPoint;
+
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
@@ -21,15 +30,42 @@ public class Player : MonoBehaviour
         stats.rng = (float)Constants.BaseStats.RNG;
     }
 
+
+
     void Update()
     {
+        Attack();
         if (joystick.Direction != Vector2.zero)
             Move();
     }
 
+
+
+    void Attack()
+    {
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider enemy in hitEnemies)
+        {
+            Debug.Log("Golpeado por espada" + enemy.name);
+        }
+
+
+    }
     void Move()
     {
         transform.eulerAngles = new Vector3(0, Mathf.Atan2(joystick.Vertical, -joystick.Horizontal) * 180 / Mathf.PI, 0);
         rigi.MovePosition(transform.position + (new Vector3(joystick.Vertical, 0, -joystick.Horizontal) * Time.fixedDeltaTime * stats.spd));
     }
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+
 }
