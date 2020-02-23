5e3d6e1e6cdbf89fd6dab236e0559a21d275b854
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-02-23 17:58:45 -0600

    camera behaviour
    Desplazamiento de camara y un objeto vacio en el prefab

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
+    //Posicion offset de la camara
     public Transform player;
     public Vector3 offset;
     Vector3 dirOffset;
 
-    private void LateUpdate() => transform.position = player.position + offset + dirOffset;
+    //0.04 o 0.07f mientras mas alto sea el smootsh speed mas brusco es el deslizamiento de la camera
+    public float smoothSpeed = 0.07f;
+
+    //private void LateUpdate() => transform.position = player.position + offset;//+ dirOffset;
+    private void Start()
+    {
+        dirOffset = new Vector3(player.position.x + 10, player.position.y, player.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset ;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        //transform.LookAt(player);
+       
+        
+    }
 }
