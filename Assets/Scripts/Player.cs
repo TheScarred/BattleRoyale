43b0c107fdad16b5fdc5cdbc903f49f852ebbc1a
TheScarred <thescarred@outlook.com>
2020-02-22 19:47:27 -0600

    Camera Behaviour
    y demas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,37 +6,30 @@ public class Player : MonoBehaviour
 {
     public Joystick joystick;
     public PlayerStats stats;
+    public Rigidbody rigi;
     public GameObject[] characters;
 
     void Start()
     {
+        joystick = FindObjectOfType<Joystick>();
+        rigi = GetComponent<Rigidbody>();
         stats = ScriptableObject.CreateInstance<PlayerStats>();
+
         stats.hp = (float)Constants.BaseStats.HP;
         stats.atk = (float)Constants.BaseStats.ATK;
         stats.spd = (float)Constants.BaseStats.SPD;
         stats.rng = (float)Constants.BaseStats.RNG;
     }
 
-    private void Awake()
-    {
-        
-    }
-
     void Update()
     {
-        /*if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Debug.Log("Touch! X: " + touch.position.x);
-        }*/
-
+        if (joystick.Direction != Vector2.zero)
+            Move();
     }
 
-    void Touch()
+    void Move()
     {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-        }
+        transform.eulerAngles = new Vector3(0, Mathf.Atan2(joystick.Vertical, -joystick.Horizontal) * 180 / Mathf.PI, 0);
+        rigi.MovePosition(transform.position + (new Vector3(joystick.Vertical, 0, -joystick.Horizontal) * Time.fixedDeltaTime * stats.spd));
     }
 }
