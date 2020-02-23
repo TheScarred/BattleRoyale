dc62165046a6ac6b066476e13b4543f43b5bfde6
TheScarred <thescarred@outlook.com>
2020-02-23 02:38:50 -0600

    Moving Photon Stuff & PUN2
    making singletons, SOs & rooms

---
+++ Assets/Scripts/Networking/Room.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Room : MonoBehaviourPunCallbacks
+{
+    public void CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom("Basic", options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room successfully!");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed: " + message);
+    }
+
+}
