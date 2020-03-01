77f2595fb3c42059ff34fa78c26e9f185c8e5627
HellAngeal <persia.2000@hotmail.com>
2020-02-29 19:46:19 -0600

    Transicion de pantallas

---
+++ Assets/Scripts/SceneManagerT.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerT : MonoBehaviour
+{
+    public static SceneManagerT instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+    public void GameScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void ShopScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void CustomizeScene()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void CharacterSelectScene()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void SettingsScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void MainMenuScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
