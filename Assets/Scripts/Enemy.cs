ecd10ae40f6553082efe2daa62771d699f2e67ec
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-02-29 23:41:24 -0600

    Animacion Muerte Enemy
    Se hizo un enemykit y se le puso un arreglo de animaciones de death

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,15 @@ public class Enemy : MonoBehaviour
 {
     public EnemyStats stats;
 
+    public bool vivo = true;
+
+    #region VARIABLES DE ANIMACION
+    [SerializeField]
+    AnimationKit enemyAnimKit;
+    [SerializeField]
+    Animator animator;
+    #endregion
+
     void Start()
     {
         stats = ScriptableObject.CreateInstance<EnemyStats>();
@@ -14,12 +23,24 @@ public class Enemy : MonoBehaviour
         stats.atk = (float)Constants.BaseEnemyStats.ATK;
         stats.spd = (float)Constants.BaseEnemyStats.SPD;
     }
-
+    #region Funcion Muerte enemigo
+    void Muerte()
+    {
+        vivo = false;
+        int numerorandom = Random.Range(0, 3);
+        Debug.Log(numerorandom);
+        animator.Play(enemyAnimKit.death[numerorandom].name);
+    }
+    public void FinishAnimationDeath()
+    {
+        vivo = false;//ya se murio xdxd
+    }
+    #endregion
     void Update()
     {
-        if(stats.hp<=0)
+        if(stats.hp<=0&&vivo)
         {
-            Debug.Log("Muerte");
+            Muerte();
         }
     }
 }
