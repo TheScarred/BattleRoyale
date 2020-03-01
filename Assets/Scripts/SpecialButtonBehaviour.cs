f6628ad523809f3a415f5d0a69834560d77552f9
TheScarred <thescarred@outlook.com>
2020-03-01 00:30:35 -0600

    Habilidad Especial 1 y 2 con COOLDOWN!
    WUU

--- Assets/Scripts/SpecialButtonBehaviour.cs
+++ Assets/Scripts/SpecialButtonBehaviour.cs
@@ -5,23 +5,31 @@ using UnityEngine.UI;
 
 public class SpecialButtonBehaviour : MonoBehaviour
 {
+    public PlayerController player;
     public Swipe swipeControls;
     public Button specialButton;
+    Text buttonText;
+    Image cooldown;
+
+    private float[] cooldowns;
 
     [SerializeField]
     private byte currentSpecial = 0;
-
     private byte CurrentSpecial { get { return currentSpecial; } set { currentSpecial = value; } }
 
     private void Start()
     {
-        
+        buttonText = specialButton.GetComponentInChildren<Text>();
+        cooldown = buttonText.GetComponentInChildren<Image>();
+        cooldown.fillAmount = 0;
+        cooldowns = new float[player.specials.Length];
     }
 
     void Update()
     {
-        if (swipeControls.SwipeLeft)
-            Debug.Log("Swipe Left");
+        ButtonGesture();
+
+        StartCoroutine(UpdateCooldowns());
     }
 
     public void BeginDrag()
@@ -29,6 +37,67 @@ public class SpecialButtonBehaviour : MonoBehaviour
         Debug.Log("Begin Drag");
     }
 
+    public void UseSpecial()
+    {
+        player.UseSpecial(currentSpecial);
+        cooldowns[currentSpecial] = player.specials[currentSpecial].cooldown;
+        specialButton.interactable = false;
+    }
 
+    public void ButtonGesture()
+    {
+        if (swipeControls.SwipeRight)
+        {
+            if (CurrentSpecial > 0)
+                CurrentSpecial--;
+            else
+                CurrentSpecial = (byte)(player.specials.Length - 1);
+
+            UpdateButton();
+        }
+        else if (swipeControls.SwipeLeft)
+        {
+            if (CurrentSpecial < player.specials.Length - 1)
+                CurrentSpecial++;
+            else
+                CurrentSpecial = 0;
+
+            UpdateButton();
+        }
+    }
+
+    public void UpdateButton()
+    {
+        if (cooldowns[CurrentSpecial] <= 0)
+            specialButton.interactable = true;
+        else
+            specialButton.interactable = false;
+
+        buttonText.text = player.specials[CurrentSpecial].specialName;
+        cooldown.fillAmount = cooldowns[CurrentSpecial];
+
+    }
+
+    public IEnumerator UpdateCooldowns()
+    {
+        for (byte i = 0; i < cooldowns.Length; i++)
+            if (cooldowns[i] > 0)
+            {
+                cooldowns[i] -= Time.deltaTime;
+
+                if (cooldowns[i] < 0)
+                    cooldowns[i] = 0;
+
+                if (i == CurrentSpecial)
+                {
+                    cooldown.fillAmount = cooldowns[i] / player.specials[i].cooldown;
+
+                    if (cooldowns[i] == 0)
+                        specialButton.interactable = true;
+                }
+            }
+
+        yield return null;
+    }
 
 }
