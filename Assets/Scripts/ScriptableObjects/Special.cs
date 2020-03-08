f21297141f08c06bee0b576ba0d71e907602aa34
HellAngeal <persia.2000@hotmail.com>
2020-03-08 17:35:55 -0600

    SwordBeam Ability

---
+++ Assets/Scripts/ScriptableObjects/Special.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Special : ScriptableObject
+{
+    public int id;
+    public float cooldown;
+    public string specialName;
+    public Constants.AttackType type;
+
+    public void Use()
+    {
+        Debug.Log("Used: " + specialName);
+    }
+
+    public void Upgrade()
+    {
+        Debug.Log("Upgraded: " + specialName);
+    }
+    public virtual void Fire(GameObject emitter)
+    {
+        
+    }
+}
